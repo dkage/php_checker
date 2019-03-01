a44a821b0ac2d8e3701cdadcc3d8ab52ab2fd2ba
Hector Rocha Margittay <hectormargittay@live.com>
2019-03-01 09:21:51 -0300

    Updated comments

--- report_project.py
+++ report_project.py
@@ -6,7 +6,7 @@ from termcolor import colored
 
 
 def get_iterator_all_files_name(dir_path):
-    """Return array with all files in directory, and another array with all subdirs in directory"""
+    # Return array with all files in directory, and another array with all subdirs in directory
     files_in_dir = os.listdir(dir_path)
     files_in_dir.sort()
 
@@ -14,18 +14,18 @@ def get_iterator_all_files_name(dir_path):
     ignore = ['.git', '.gitignore', '.idea', '.gitlab-ci.yml']
     files_in_dir = list(filter(lambda f: not(f in ignore) and not os.path.isdir(dir_path + f), files_in_dir))
 
-    # print(files_in_dir)
     return files_in_dir
 
 
 def php_syntax_test(php_path, php_file):
-    """Checks php file for syntax errors using php -lint"""
+    # Check php file for syntax errors using php -lint
     cli_command = 'php -l ' + php_path + php_file + ' >/dev/null 2>&1'
     lint = os.system(cli_command)
     return lint != 0
 
+# INPUT
 
-# Check if has a
+# Check if has the path parameter has been given
 if len(sys.argv) == 1:
     print('Insira o caminho absoluto do diretório a ser analisado')
     sys.exit()
@@ -33,15 +33,17 @@ if len(sys.argv) == 1:
 # Analyzed project root folder - absolute path
 path = sys.argv[1]
 
-# Checks if the directory exists
+# Check if the directory exists
 if not os.path.isdir(path):
     print('Diretório não encontrado')
     sys.exit()
 
+# END INPUT
+
 print('Analisando arquivos...')
 
 
-# Creates array with every directory and subdirs and remove ignored directories
+# Create array with every directory and subdirs and remove ignored directories
 dirs_array = []
 for x in os.walk(path):
     cur_path = x[0]
@@ -55,13 +57,15 @@ for x in os.walk(path):
 all_project_files = []
 all_php_filepaths = []
 all_php_filenames = []
+
 path_dict = dict()
+
 # Generate array with every filename
 for directory in dirs_array:
     all_files_in_dir = get_iterator_all_files_name(directory)
     all_project_files += all_files_in_dir
 
-    # pega todos os arquivos do tipo PHP
+    # Get all PHP files and filenames
     for file in all_files_in_dir:
         ext = file.rsplit('.', 1)[-1]
         if ext == 'php':
@@ -70,6 +74,8 @@ for directory in dirs_array:
 
 all_project_files.sort()
 all_php_filepaths.sort()
+
+# Create a set with all PHP files without a found reference
 unref_php_filenames = list(set(all_php_filenames))
 unref_php_filenames.sort()
 
@@ -78,6 +84,7 @@ for file in all_php_filepaths:
         file_string = open_file.read()
 
         for filename in unref_php_filenames:
+            # Remove filename from the list if a reference is found to optimize the next iterations
             if filename in file_string:
                 unref_php_filenames.remove(filename)
 
@@ -85,37 +92,51 @@ for file in all_php_filepaths:
 tree_dict = dict()
 for directory in dirs_array:
     files_dict = dict()
+    
     all_files_in_dir = get_iterator_all_files_name(directory)
     for each_file in all_files_in_dir:
+        # Ignore files without '.php' in the filename
         if '.php' not in each_file:
             continue
-
+        
+        # Check if the file has an unvalid php extension
         check_ext = each_file.rsplit('.php', 1)[-1] != ''
+        
         syntax = False
         pg_connect = False
         uploaded = False
         unref = False
 
+        # Get file extension
         extension = each_file.rsplit('.', 1)[-1]  # recebe tipo da extensao
 
         if extension == 'php':
-            # Loads file to variable as string
+            # Load file to variable as string
             with open(directory + each_file, 'r', encoding='utf-8', errors='ignore') as myfile:
                 loaded_file = myfile.read()
 
-                # Checks for comment lines and comment blocks and remove then to search only in code being used
+                # Check for comment lines and comment blocks and remove then to search only in code being used
                 treated_file = re.sub(r"((/\*).*(\*/))|((/\*).*$)|(//[^\n]*)", '', loaded_file, flags=re.DOTALL)
 
+                # Perform a syntax test on the file
                 syntax = php_syntax_test(directory, each_file)
+                
+                # Search for uses of the 'pg_connect' function
                 pg_connect = 'pg_connect' in treated_file
+                
+                # Search for uses of the 'move_uploaded_file' function
                 uploaded = 'move_uploaded_file' in treated_file
 
+            # Check if file in the unreferenced files list
             unref = each_file in unref_php_filenames
 
+        # Store tests results
         files_dict[each_file] = [check_ext, syntax, pg_connect, uploaded, unref]
 
     tree_dict[directory] = files_dict
 
+
+# OUTPUT
 
 for directory in tree_dict:
     # Hide directories without PHP files
@@ -128,6 +149,7 @@ for directory in tree_dict:
         errors = []
         warnings = []
 
+        # Create messages based on the tests results
         results = tree_dict[directory][file]
         if results[0]:
             errors.append('Extensão PHP inválida')
@@ -156,7 +178,4 @@ for directory in tree_dict:
         for warning in warnings:
             print('      ' + warning)
 
-
-
-
-
+# END OUTPUT
