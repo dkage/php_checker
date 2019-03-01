319d5a52dadd1eef41c4bcc38d4bb26bab0001dc
Danilo Cunha <danilokage@gmail.com>
2019-02-28 23:57:00 -0300

    V2

--- report_project.py
+++ report_project.py
@@ -1,25 +1,162 @@
+import io
 import os
-
-path = '/Users/danilo/Sites/localhost/wwwroot/educfinAdm/'
-relative_path = '/'
+import re
+import sys
+from termcolor import colored
 
 
 def get_iterator_all_files_name(dir_path):
-    files_path = os.listdir(dir_path)
-    # dirs_path = [x[0] for x in os.walk(path)]
-    return files_path
+    """Return array with all files in directory, and another array with all subdirs in directory"""
+    files_in_dir = os.listdir(dir_path)
+    files_in_dir.sort()
+
+    # Remove unwanted filenames and subdirectories
+    ignore = ['.git', '.gitignore', '.idea', '.gitlab-ci.yml']
+    files_in_dir = list(filter(lambda f: not(f in ignore) and not os.path.isdir(dir_path + f), files_in_dir))
+
+    # print(files_in_dir)
+    return files_in_dir
 
 
-files = get_iterator_all_files_name(path)
+def php_syntax_test(php_path, php_file):
+    """Checks php file for syntax errors using php -lint"""
+    cli_command = 'php -l ' + php_path + php_file + ' >/dev/null 2>&1'
+    lint = os.system(cli_command)
+    return lint != 0
 
-files.sort()
-for file in files:
-    if os.path.isdir(path + file) or file == '.gitlab-ci.yml':
+
+# Check if has a
+if len(sys.argv) == 1:
+    print('Insira o caminho absoluto do diretório a ser analisado')
+    sys.exit()
+
+# Analyzed project root folder - absolute path
+path = sys.argv[1]
+
+# Checks if the directory exists
+if not os.path.isdir(path):
+    print('Diretório não encontrado')
+    sys.exit()
+
+print('Analisando arquivos...')
+
+
+# Creates array with every directory and subdirs and remove ignored directories
+dirs_array = []
+for x in os.walk(path):
+    cur_path = x[0]
+    if '.git' in cur_path:
         continue
-    extension = file.rsplit('.', 1)[-1]
-    if extension == 'php':
-        cli_command = 'php -l ' + path + file + ' >/dev/null 2>&1'
-        # cli_command = 'php -l ' + file
-        lint = os.system(cli_command)
-        output = relative_path + file + '   ' + ('OK' if lint == 0 else 'SYNTAX ERROR')
-    print(output)
+    if '.idea' in cur_path:
+        continue
+
+    dirs_array.append(cur_path + '/')
+
+all_project_files = []
+all_php_filepaths = []
+all_php_filenames = []
+path_dict = dict()
+# Generate array with every filename
+for directory in dirs_array:
+    all_files_in_dir = get_iterator_all_files_name(directory)
+    all_project_files += all_files_in_dir
+
+    # pega todos os arquivos do tipo PHP
+    for file in all_files_in_dir:
+        ext = file.rsplit('.', 1)[-1]
+        if ext == 'php':
+            all_php_filepaths.append(directory + file)
+            all_php_filenames.append(file)
+
+all_project_files.sort()
+all_php_filepaths.sort()
+unref_php_filenames = list(set(all_php_filenames))
+unref_php_filenames.sort()
+
+for file in all_php_filepaths:
+    with open(file, 'r',  encoding='utf-8', errors='ignore') as open_file:
+        file_string = open_file.read()
+
+        for filename in unref_php_filenames:
+            if filename in file_string:
+                unref_php_filenames.remove(filename)
+
+
+tree_dict = dict()
+for directory in dirs_array:
+    files_dict = dict()
+    all_files_in_dir = get_iterator_all_files_name(directory)
+    for each_file in all_files_in_dir:
+        if '.php' not in each_file:
+            continue
+
+        check_ext = each_file.rsplit('.php', 1)[-1] != ''
+        syntax = False
+        pg_connect = False
+        uploaded = False
+        unref = False
+
+        extension = each_file.rsplit('.', 1)[-1]  # recebe tipo da extensao
+
+        if extension == 'php':
+            # Loads file to variable as string
+            with open(directory + each_file, 'r', encoding='utf-8', errors='ignore') as myfile:
+                loaded_file = myfile.read()
+
+                # Checks for comment lines and comment blocks and remove then to search only in code being used
+                treated_file = re.sub(r"((/\*).*(\*/))|((/\*).*$)|(//[^\n]*)", '', loaded_file, flags=re.DOTALL)
+
+                syntax = php_syntax_test(directory, each_file)
+                pg_connect = 'pg_connect' in treated_file
+                uploaded = 'move_uploaded_file' in treated_file
+
+            unref = each_file in unref_php_filenames
+
+        files_dict[each_file] = [check_ext, syntax, pg_connect, uploaded, unref]
+
+    tree_dict[directory] = files_dict
+
+
+for directory in tree_dict:
+    # Hide directories without PHP files
+    if not tree_dict[directory]:
+        continue
+
+    # Print directory name
+    print(colored(directory, 'blue'))
+    for file in tree_dict[directory]:
+        errors = []
+        warnings = []
+
+        results = tree_dict[directory][file]
+        if results[0]:
+            errors.append('Extensão PHP inválida')
+        if results[1]:
+            errors.append('Erro na sintáxe')
+        if results[2]:
+            warnings.append('Utiliza função pg_connect')
+        if results[3]:
+            warnings.append('Salva arquivo no servidor')
+        if results[4]:
+            errors.append('Arquivo não referenciado')
+
+        # Set file status color
+        file_color = 'green'
+        if errors:
+            file_color = 'red'
+        elif warnings:
+            file_color = 'yellow'
+        # Print filename
+        print(colored('  - ' + file, file_color))
+
+        # Print errors messages
+        for error in errors:
+            print('      ' + error)
+        # Print warnings messages
+        for warning in warnings:
+            print('      ' + warning)
+
+
+
+
+
--- unref_tests.py
+++ unref_tests.py
@@ -1,32 +1,97 @@
 import os
-
-
+#
+#
 path = '/Users/danilo/Sites/localhost/wwwroot/educfinAdm/'
+#
+#
+# def get_iterator_all_files_name(dir_path):
+#     files_path = os.listdir(dir_path)
+#     dirs_path = [x[0] for x in os.walk(path)]
+#     return files_path, dirs_path
+#
+#
+# def print_files(prefix_path, filenames_array):
+#     for file in filenames_array:
+#         if os.path.isdir(prefix_path+file):
+#             continue
+#         print(file)
+#
+#
+# filenames, dirs = get_iterator_all_files_name(path)
+#
+# print_files(path, filenames)
+#
+#
+# print('\n--------------------\n')
+#
+# for directory in dirs:
+#     print(directory)
+#     if '.git' in directory or directory == 'js' or directory == 'css':
+#         continue
+#     print(directory)
+#
 
+
+# for x in os.walk(path):
+#     print(x)
+
+#
+# def getListOfFiles(dirName):
+#     # create a list of file and sub directories
+#     # names in the given directory
+#     listOfFile = os.listdir(dirName)
+#     allFiles = list()
+#     # Iterate over all the entries
+#     for entry in listOfFile:
+#         # Create full path
+#         fullPath = os.path.join(dirName, entry)
+#         # If entry is a directory then get the list of files in this directory
+#         if os.path.isdir(fullPath):
+#             allFiles = allFiles + getListOfFiles(fullPath)
+#         else:
+#             allFiles.append(fullPath)
+#
+#     return allFiles
+
+def php_syntax_test(php_file):
+    # TODO get path
+    """Checks php file for syntax errors using php -lint"""
+    cli_command = 'php -l ' + path + php_file + ' >/dev/null 2>&1'
+    lint = os.system(cli_command)
+    return 'OK' if lint == 0 else 'SYNTAX ERROR'
+    # output = relative_path + file + '   ' + ('OK' if lint == 0 else 'SYNTAX ERROR')
 
 def get_iterator_all_files_name(dir_path):
-    files_path = os.listdir(dir_path)
-    dirs_path = [x[0] for x in os.walk(path)]
-    return files_path, dirs_path
+    """Return array with all files in directory, and another array with all subdirs in directory"""
+    files_in_dir = os.listdir(dir_path)
+    files_in_dir.sort()
+
+    # Remove unwanted filenames
+    files_to_ignore = ['.git', '.gitignore', '.idea', '.gitlab-ci.yml']
+    files_in_dir = list(filter(lambda x: x not in files_to_ignore, files_in_dir))
+
+    subdirs_in_dir = []
+    for name in files_in_dir:
+        if os.path.isdir(path + name):
+            subdirs_in_dir.append(name)
+            files_in_dir.remove(name)
+
+    return files_in_dir, subdirs_in_dir
 
 
-def print_files(prefix_path, filenames_array):
-    for file in filenames_array:
-        if os.path.isdir(prefix_path+file):
-            continue
-        print(file)
+dirs_path = [x[0] for x in os.walk(path)]
+# paths_to_ignore = ['.git', '.gitignore', '.idea', '.gitlab-ci.yml']
+# dirs_path = list(filter(lambda x: x not in paths_to_ignore, dirs_path))
 
-
-filenames, dirs = get_iterator_all_files_name(path)
-
-print_files(path, filenames)
-
-
-print('\n--------------------\n')
-
-for directory in dirs:
-    print(directory)
-    if '.git' in directory or directory == 'js' or directory == 'css':
+for each_path in dirs_path:
+    if '.git' in each_path or '.idea' in each_path:
         continue
-    print(directory)
-
+    print(each_path)
+    files, dirs = get_iterator_all_files_name(each_path)
+    for each_file in files:
+        extension = each_file.rsplit('.', 1)[-1]
+        if extension == 'php':
+            output = each_file, php_syntax_test(each_file)
+            print(output)
+            print(each_file)
+    print('\n\n')
